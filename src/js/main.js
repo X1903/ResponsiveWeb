19b001a557dc8bab1abfbb651c48a9fbb3209d60
xin <xxin1903@163.com>
2018-03-16 10:04:32 +0800

    响应式布局_理财网站项目页面

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016/9/1.
+ */
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true // 鼠标是否控制停止
+    });
+});
